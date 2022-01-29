9cc49f727ee3849dfca2a6db1d5ae5af96d26eb2
Eric Macy <eric_macy@nonik.com>
2022-01-29 13:45:58 -0500

    fix triggers

--- 2022BasicMove/src/main/java/frc/robot/Robot.java
+++ 2022BasicMove/src/main/java/frc/robot/Robot.java
@@ -4,14 +4,34 @@
 
 package frc.robot;
 
+//DEFAULT IMPORTS! DO NOT REMOVE!
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+//Basic imports, needed for essential robot funtions
+//*note*, this does NOT include drive encoders (import spark and other drive encoders seperately)
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.AnalogInput;
+
+//Movement Encoder Imports / motor controllers
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+//import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
@@ -21,7 +41,8 @@ public class Robot extends TimedRobot {
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
@@ -29,26 +50,45 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+    motor2.setInverted(true);
+
+    /*
+     * m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
+     * m_leftStick = new Joystick(0);
+     * m_rightStick = new Joystick(1);
+     */
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and
+   * test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+  }
 
   /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different
+   * autonomous modes using the dashboard. The sendable chooser code works with
+   * the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+   * chooser code and
+   * uncomment the getString line to get the auto name from the text box below the
+   * Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure
+   * below with additional strings. If using the SendableChooser make sure to add
+   * them to the
    * chooser code above as well.
    */
   @Override
@@ -74,25 +114,53 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
+  public void teleopInit() {
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
+  /*
+   * Defines Drive Terms, potentially needed.
+   * private DifferentialDrive m_myRobot;
+   * private Joystick m_leftStick;
+   * private Joystick m_rightStick;
+   */
 
-  /** This function is called periodically during test mode. */
+  /* Drive Train Motors */
+  private final MotorController motor1 = new PWMVictorSPX(0);
+  private final MotorController motor2 = new PWMVictorSPX(1);
+  private final MotorController motor3 = new PWMVictorSPX(2);
+
+  /* Joysticks */
+  private static Joystick controller_0 = new Joystick(0);
+
+  /** This function is called periodically during operator control. */
   @Override
-  public void testPeriodic() {}
+  public void teleopPeriodic() {
+
+    /* Drive Robot - Axis 0 & 1 (X & Y Left Joystick) */
+    double speed1 = 1 * controller_0.getRawAxis(2); /* negative is right */
+    double speed2 = -1 * controller_0.getRawAxis(3); /* negative is forwardard */
+
+    /* Controller Deadband 10% */
+    if (Math.abs(speed2) < 0.10) {
+      speed2 = 0;
+    }
+    if (Math.abs(speed1) < 0.10) {
+      speed1 = 0;
+    }
+
+    motor1.set(speed1);
+    motor2.set(speed2);
+
+
+  }
 }
